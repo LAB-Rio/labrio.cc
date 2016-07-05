2f070532cdd89f51c2d8f1c9755b0097d03b780f
Frederick Silva <fredericksilva@outlook.com>
2016-07-05 16:44:13 +0000

    imersao api flirck

--- public/assets/javascripts/main.js
+++ public/assets/javascripts/main.js
@@ -1,3 +1,4 @@
+console.log("LAB.RIO");
 function scrollBanner() {
   var scrollPos;
   var headerText = document.querySelector('.header-paralax h1');
@@ -10,3 +11,34 @@ function scrollBanner() {
 }
 
 window.addEventListener('scroll', scrollBanner);
+
+//FlickrAPi
+$(document).ready(function(){
+  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=129889428@N02&tags=imersao2016&format=json&jsoncallback=?", function(data){
+  		$.each(data.items, function(i,item){
+  			 var image = (item.media.m).replace("_m.jpg", "_d.jpg");
+         var title = item.title;
+         if(i<21){
+  			    $("<div>")
+            .attr({"style":"background-image: url("+image+")","class":"flickr-image", "data-title":title})
+             .appendTo(".imersao2016").wrap("<a href='" + item.link + "'></a>");
+  			    }    
+  		});
+  });
+  
+  
+  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=129889428@N02&tags=imersao2015&format=json&jsoncallback=?", function(data){
+  		$.each(data.items, function(i,item){
+  			 var image = (item.media.m).replace("_m.jpg", "_d.jpg");
+         var title = item.title;
+         if(i<21){
+  			    $("<div>")
+            .attr({"style":"background-image: url("+image+")","class":"flickr-image", "data-title":title})
+             .appendTo(".imersao2015").wrap("<a href='" + item.link + "'></a>");
+  			    }    
+  		});
+  });
+  
+  
+});
+
