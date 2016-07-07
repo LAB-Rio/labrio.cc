f6f1ce76e7c54d9b45fbd217cdc06632036f1164
Frederick Silva <fredericksilva@outlook.com>
2016-07-07 15:51:12 +0000

    testimonials

--- public/assets/javascripts/main.js
+++ public/assets/javascripts/main.js
@@ -33,23 +33,11 @@ window.addEventListener('scroll', scrollBanner);
   		$.each(data.items, function(i,item){
   			 var image = (item.media.m).replace("_m.jpg", "_d.jpg");
          var title = item.title;
+         var blank = "_blank";
          if(i<21){
   			    $("<div>")
             .attr({"style":"background-image: url("+image+")","class":"flickr-image", "data-title":title})
-             .appendTo(".imersao2015").wrap("<a href='" + item.link + "'></a>");
-  			    }    
-  		});
-  });
-  
-  
-  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=129889428@N02&tags=imersao2015&format=json&jsoncallback=?", function(data){
-  		$.each(data.items, function(i,item){
-  			 var image = (item.media.m).replace("_m.jpg", "_d.jpg");
-         var title = item.title;
-         if(i<21){
-  			    $("<div>")
-            .attr({"style":"background-image: url("+image+")","class":"flickr-image", "data-title":title})
-             .appendTo(".imersao2015").wrap("<a href='" + item.link + "'></a>");
+             .appendTo(".imersao2015").wrap("<a target='" +blank+ "'href='" + item.link + "'></a>");
   			    }    
   		});
   });
@@ -59,6 +47,7 @@ window.addEventListener('scroll', scrollBanner);
   		$.each(data.items, function(i,item){
   			 var image = (item.media.m).replace("_m.jpg", "_d.jpg");
          var title = item.title;
+         var blank = "_blank";
          if(i<21){
   			    $("<div>")
             .attr({"style":"background-image: url("+image+")","class":"flickr-image", "data-title":title})
